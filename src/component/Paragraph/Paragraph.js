0c8ef1881132493b5c3aaada3b7b6e2a02000e61
Janetta Kataoeva <jkattoeva@gmail.com>
2022-04-01 14:45:22 +0600

    destruction

---
+++ src/component/Paragraph/Paragraph.js
@@ -0,0 +1,9 @@
+function Pragraph({ size, color, children }) {
+  const style = {
+    fontSize: size,
+    color: color
+  };
+  return <p style={style}>{children}</p>;
+}
+
+export default Pragraph;
