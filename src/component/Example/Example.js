299e6d8835c4dc74038347b22f7cee12c51d46ee
Janetta Kataoeva <jkattoeva@gmail.com>
2022-04-03 14:31:58 +0600

    created new input

--- src/component/Example/Example.js
+++ src/component/Example/Example.js
@@ -5,6 +5,11 @@ function Example() {
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [firstNumber, setFirstNumber] = useState("");
+  const [lastnNumber, setLastNumber] = useState("");
+  const [res, setRes] = useState("");
+
+
 
 
   const onFirstNameChange = (event) => {
@@ -14,7 +19,16 @@ function Example() {
     setLastName(event.target.value);
   }
 
-
+  const onFirstNumberChange = (event) =>{
+    setFirstNumber(event.target.value);
+  }
+  
+  const onLastNumberChange = (event) =>{
+    setLastNumber(event.target.value);
+  }
+  const result = () => {
+   const setRes =  firstNumber + lastnNumber
+  }
 
   return (
     <div className="Example">
@@ -27,6 +41,18 @@ function Example() {
         last name:
         <input className="lastInput" type="text" onChange={onLastNameChange}/>
       </label>
+      <div className="Example2">
+      {firstNumber} {lastnNumber}
+      <label>
+        a:
+        <input className="firstInput" type="text" onChange={onFirstNumberChange}/>
+      </label>
+      <label>
+        b:
+        <input className="lastInput" type="text" onChange={onLastNumberChange}/>
+      </label>
+      {res}
+      </div>
     </div>
   );
 }
